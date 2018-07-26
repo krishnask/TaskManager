5956d7915f5885fea450b7241040876a38b99c63
krishnask <kkumar.srini@gmail.com>
2018-07-26 06:17:23 +0530

    Added UdateTask
    Added UdateTask

--- taskmanager-ui/src/app/app.module.ts
+++ taskmanager-ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateTaskComponent
   ],
   imports: [
     BrowserModule
